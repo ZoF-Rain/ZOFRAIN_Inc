11daea7c02f46b132f5fa61cc056e313435cd6f1
ZoF-Rain <zavarinmax@gmail.com>
2023-12-03 02:26:05 +0300

    99832

--- scripts/mediaFooter.js
+++ scripts/mediaFooter.js
@@ -4,12 +4,16 @@ mediaFooter.addEventListener('click', function(event){
     let button = event.target;
     if(button.tagName.toLowerCase() === 'button') {
         let ul = button.nextElementSibling;
-        if(ul && ul.tagName.toLowerCase() === 'ul' && ul.style.display != 'block') {
+        let arrow = button.querySelector('.butar');
+        if(ul && ul.tagName.toLowerCase() === 'ul') {
+            if(ul.style.display != 'block') {
                 ul.style.display = 'block';
+                arrow.src = "images/arrowdown.png";
             }
             else{
                 ul.style.display = 'none';
+                arrow.src = "images/arrow.png";
             }
         }
     }
-)
+})
